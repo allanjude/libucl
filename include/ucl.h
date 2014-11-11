d243d21dd2857d737786478677d07e16aba71095
Allan Jude <allanjude@freebsd.org>
2014-11-11 16:36:19 -0500

    Clarify documentation

--- include/ucl.h
+++ include/ucl.h
@@ -355,7 +355,7 @@ UCL_EXTERN bool ucl_object_replace_key (ucl_object_t *top, ucl_object_t *elt,
  * @param top destination object (must be of type UCL_OBJECT)
  * @param elt element to insert (must be of type UCL_OBJECT)
  * @param copy copy rather than link the elements
- * @return true if all keys have been merged
+ * @return true if objects were merged
  */
 UCL_EXTERN bool ucl_object_merge (ucl_object_t *top, ucl_object_t *elt, bool copy);
 
